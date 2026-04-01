f2dc1ed90c95f1b8060f5a21d8faa28f1315c1b4
alejandroE345 <alejandroencinas345@gmail.com>
2026-04-01 01:09:13 +0000

    complete

--- script.js
+++ script.js
@@ -12,14 +12,43 @@ function start() {
     // Adding the element to the page
     document.body.appendChild(heading);
     // Aquiring the user's name to add to the attendance list
+    //Person 1
     let userName = prompt("What is your name?");
     let userLastName = prompt("What is your last name?");
     console.log(userName);
     console.log(userLastName);
     addName(userLastName, userName);
     addSeparator();
+    //Person 2
     userName = prompt("What is your name?");
-    userLastName = prompt();
+    userLastName = prompt("What is your last name?");
+    addName(userLastName, userName);
+    console.log(userName);
+    console.log(userLastName);
+    addSeparator();
+    //Person 3
+    userName = prompt("What is your name?");
+    userLastName = prompt("What is your last name?");
+    addName(userLastName, userName);
+    console.log(userName);
+    console.log(userLastName);
+    addSeparator();
+    //Person 4
+    userName = prompt("What is your name?");
+    userLastName = prompt("What is your last name?");
+    addName(userLastName, userName);
+    console.log(userName);
+    console.log(userLastName);
+    addSeparator();
+    //Person 5
+    userName = prompt("What is your name?");
+    userLastName = prompt("What is your last name?");
+    addName(userLastName, userName);
+    console.log(userName);
+    console.log(userLastName);
+    addSeparator();
+
+
 }
 // Function for the 1st exercise
     function addName(lastName, firstName) {
